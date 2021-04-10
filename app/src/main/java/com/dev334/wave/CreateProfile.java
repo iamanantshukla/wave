a6ff2d4256632bf58273ca9486277f7c9a0946ce
Anant Shukla <anantshukla1402@gmail.com>
2021-04-10 21:51:40 +0530

    Home Activity with chip navigation

--- app/src/main/java/com/dev334/wave/CreateProfile.java
+++ app/src/main/java/com/dev334/wave/CreateProfile.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+public class CreateProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+        setContentView(R.layout.activity_create_profile);
     }
 }
--- app/src/main/java/com/dev334/wave/SplashScreen.java
+++ app/src/main/java/com/dev334/wave/SplashScreen.java
@@ -1,48 +1,58 @@
 package com.dev334.wave;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SplashScreen extends AppCompatActivity {
-    private ImageView logo;
+    private static final String TAG ="SplashScreen";
     private FirebaseAuth mAuth;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        //logo=findViewById(R.id.splashLogo);
+        getSupportActionBar().hide();
+
         mAuth=FirebaseAuth.getInstance();
+        firestore= FirebaseFirestore.getInstance();
 
-
-
-        Handler mHandler= new Handler();  //Splash screen change act. delay 1 second
-        mHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-
-                if(mAuth.getCurrentUser()==null){
-                    // if No logged in User
-                    //Transition for Logo
-                    Intent sharedintent= new Intent(SplashScreen.this,EmailLogin.class);
-                    ActivityOptions options= ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this,
-                            new android.util.Pair<View, String>(logo, "logoTransition"));
-                    startActivity(sharedintent, options.toBundle());
-                }else{
-                    //directly then Opening the HomeActivity
-                    Intent i=new Intent(SplashScreen.this, HomeActivity.class);
-                    startActivity(i);
+        if(mAuth.getCurrentUser()==null){
+            Intent i=new Intent(SplashScreen.this, LoginHome.class);
+            startActivity(i);
+        }else{
+            firestore.collection("Users").document(mAuth.getCurrentUser().getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot snapshot) {
+                    if(snapshot.exists()){
+                        Intent i=new Intent(SplashScreen.this, HomeActivity.class);
+                        startActivity(i);
+                    }else{
+                        Intent i=new Intent(SplashScreen.this, CreateProfile.class);
+                        startActivity(i);
+                    }
                 }
-            }
-        },1000);
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.i(TAG, "onFailure: "+e.getMessage());
+                }
+            });
+        }
 
     }
 }
